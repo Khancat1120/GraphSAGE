979144d8ddb5b66b295ab60911ce18aa0a8643fd
Khancat1120 <105252819+Khancat1120@users.noreply.github.com>
2023-09-28 20:13:41 +0800

    Add files via upload

--- HeteroDataset.py
+++ HeteroDataset.py
@@ -8,15 +8,15 @@ import dgl
 import copy
 
 
-class HomoDataset:
+class HeteroDataset:
     def __init__(self):
         self.file_location = "dataset/"
         self.dataset_name = "movielens/"
         self.dir_to_load = self.file_location + self.dataset_name
 
         self.graph_filename = "movielen_e.csv"
-        self.train_graph_file = "movielen_train.csv"
-        self.test_graph_file = "movielen_test.csv"
+        self.train_graph_file = "new_train.csv"
+        self.test_graph_file = "new_test.csv"
 
         self.edge_label_filename = "./attributes_info/movielens_kg_label_map.csv"
         self.node_feature_filename = "./feature_vectors_v.csv"
@@ -28,29 +28,42 @@ class HomoDataset:
         self.train_graph = pd.read_csv(self.dir_to_load + self.train_graph_file)
         self.test_graph = pd.read_csv(self.dir_to_load + self.test_graph_file)
 
-        self.sources = []
-        self.sinks = []
-        self.rels = []
-        self.rels_num = 0
-
         self.graph = self.load_graph()
 
         self.item_num = 3706
         self.user_num = 6040
         self.kg_num = 21201 - self.item_num - self.user_num
+
         self.add_node_features()
 
     def load_graph(self):
-        self.sources = self.origin_graph.iloc[:, 1].values
-        self.sinks = self.origin_graph.iloc[:, 2].values
-        self.rels = self.origin_graph.iloc[:, 3].values
-        self.rels_num = len(set(self.rels))
-        graph = dgl.graph((self.sources, self.sinks))
-        return graph
+        edges_group = self.origin_graph.groupby(['label_id:int'])
+        hetero_graph_dict = {}
+        for label_id in edges_group.groups:
+            edges_of_id = edges_group.get_group(label_id)
+            srcs = edges_of_id["source_id:int"].to_numpy()
+            dsts = edges_of_id["target_id:int"].to_numpy()
+            if label_id == 0 or label_id == 1:
+                edge_type_name = ('user', str(self.edge_labels[label_id][1]), 'item')
+            elif label_id == 2 or label_id == 3:
+                edge_type_name = ('item', str(self.edge_labels[label_id][1]), 'user')
+            elif 3 < label_id < 24:
+                edge_type_name = ('item', str(self.edge_labels[label_id][1]), 'kg')
+            else:
+                edge_type_name = ('kg', str(self.edge_labels[label_id][1]), 'item')
+
+            if edge_type_name in hetero_graph_dict.keys():
+                print("already exist...")
+            else:
+                hetero_graph_dict[edge_type_name] = (torch.from_numpy(srcs), torch.from_numpy(dsts))
+
+        return dgl.heterograph(hetero_graph_dict)
 
     def add_node_features(self):
-        self.graph.ndata['h'] = torch.cat(
-            (torch.from_numpy(self.node_features[:, 1:]), torch.randn(self.kg_num, 16))).to(torch.float32)
+        self.graph.nodes['item'].data['feat'] = torch.from_numpy(self.node_features[:3706, 1:])
+        self.graph.nodes['user'].data['feat'] = torch.from_numpy(self.node_features[:, 1:])
+        self.graph.nodes['kg'].data['feat'] = torch.cat(
+            (self.graph.nodes['user'].data['feat'], torch.randn(self.kg_num, 16))).to(torch.float32)
 
     def get_train_batch(self, batch_size):
         # sampled_data = self.train_graph.sample(batch_size).values
--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import torch
 
-from HeteDataset import *
+from HeteroDataset import *
 from HomoDataset import *
-from model import *
+from HeteroModel import *
 import argparse
 import copy
 from sklearn.metrics import f1_score
+from sklearn.metrics import roc_auc_score
 
 
 def get_parameter():
     parser = argparse.ArgumentParser()
     parser.add_argument('-lr', default=0.1, type=float, help="learning rate")
     parser.add_argument('-embed_dim', default=32, type=int, help="embedding dimension")
-    parser.add_argument('-hidden_dim', default=64, type=int, help="embedding dimension")
+    parser.add_argument('-hidden_dim', default=128, type=int, help="embedding dimension")
     parser.add_argument('-reg_lambda', default=0.03, type=float, help="l2 regularization parameter")
     parser.add_argument('-batch_size', default=500, type=int, help="batch size")
     parser.add_argument('-epoch', default=100, type=int, help="batch size")
@@ -23,30 +24,35 @@ def get_parameter():
 
 if __name__ == '__main__':
     args = get_parameter()
-    dataset = HomoGraph()
+    dataset = HeteroDataset()
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model = GraphSAGE(16, args.hidden_dim, args.embed_dim).to(device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.05,
-                                 weight_decay=5e-4)  # Increased weight decay and reduced learning rate
+    model = PinSAGE(16, args.hidden_dim, args.embed_dim, dataset.graph).to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001,
+                                 weight_decay=1e-4)  # Increased weight decay and reduced learning rate
     criterion = nn.BCEWithLogitsLoss()
     graph = copy.deepcopy(dataset.graph)
-    n_features = graph.ndata['h'].shape[1]
-    k = 5
-    opt = torch.optim.Adam(model.parameters())
+
+    n_features = 16
 
     h = []
+    user = []
+    item = []
     model.train()
     print("===================Training==========================")
-    for epoch in range(1000):
-        h = model(graph, graph.ndata['h'])
-        samples = dataset.get_train_batch(1000)
+    for epoch in range(100):
+        node_dict = {node_type: graph.nodes[node_type].data['feat'].to(device).to(torch.float32) for node_type in
+                     graph.ntypes}
+        h = model(graph, node_dict)
+        samples = dataset.get_train_batch(5120)
         first_elements, second_elements, labels = zip(*samples)
         a = torch.index_select(h, dim=0, index=torch.tensor(first_elements).to(device))
         b = torch.index_select(h, dim=0, index=torch.tensor(second_elements).to(device))
+        c = (a * b).sum(dim=1)
         ground_truth = 2 * torch.tensor(labels).to(device) - 1
 
-        loss = criterion((a * b).sum(dim=1), ground_truth.to(torch.float32))
+        loss = criterion(c, ground_truth.to(torch.float32))
+        # print(loss)
         optimizer.zero_grad()  # 清零梯度
         loss.backward()  # 反向传播
         optimizer.step()  # 更新模型参数
@@ -55,9 +61,13 @@ if __name__ == '__main__':
     model.eval()
     test = dataset.get_test()
     first_elements, second_elements, labels = zip(*test)
+
     a = torch.index_select(h, dim=0, index=torch.tensor(first_elements).to(device))
     b = torch.index_select(h, dim=0, index=torch.tensor(second_elements).to(device))
     c = (a * b).sum(dim=1)
-    c = torch.where(c > 0, torch.tensor(1), torch.tensor(-1)).to('cpu')
-    ground_truth = 2 * torch.tensor(labels).to(device).to('cpu') - 1
-    print(f1_score(ground_truth, c))
+    # c = torch.where(c > 0, torch.tensor(1), torch.tensor(-1)).to('cpu')
+    # ground_truth = 2 * torch.tensor(labels).to(device).to('cpu') - 1
+    c = (c.to('cpu') + 1) / 2
+    # c = torch.sigmoid(c).to('cpu')
+    ground_truth = torch.tensor(labels).to(device).to('cpu')
+    print(roc_auc_score(ground_truth.detach().numpy(), c.detach().numpy()))
